f4951bfebb415139247f4f2df8a476fefa13b0cc
DEEPAK KUMAR BEHERA <>
2019-04-20 12:55:05 +0530

    new...commit

--- src/Test/LoginTest.java
+++ src/Test/LoginTest.java
@@ -9,7 +9,7 @@ import UI_Components.LoginComponent;
 public class LoginTest extends AbstractTest
 	{
 		@Test(description="Verfiy Login",priority = 1)//you have set priority as you are working on different test
-		//if not set above priority ,it will throw an Error.
+		//if not set above priority ,it will throw an Error....
 		public void verifyLogin()
 		{
 			assertTrue(loginComponent.verfiyLoginFunc(), "Verify the Login..."); //loginComponent :object name
